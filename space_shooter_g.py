5ba9f2b32e958e038d48be47f5e49def93863f96
gz6t9crf <gz6t9crf@schullogin.de>
2026-05-10 15:11:15 +0200

    Ich habe Helenas Änderungen in mein Code übertragen, damit die Meteoriten auch bei mir fallen.

--- space_shooter_g.py
+++ space_shooter_g.py
@@ -1,11 +1,13 @@
 import pgzrun
 import pygame
+import random
 
 # Globale Variablen
 WIDTH = 1280
 HEIGHT = 720
+GRAVITY = 0.02
 
-MOVE_SPEED= 5
+MOVE_SPEED= 10
 
 background = None
 
@@ -13,7 +15,22 @@ background = None
 ship  = Actor("spaceships_007.png", anchor=("center","bottom"))
 ship.midbottom  = (600,550)
 ship.angle = 180  # Schiff umdrehen, damit es auf dem Kopf steht
-meteors= [Actor("meteorbrown_big1.png", topleft=(100, 100)), Actor("meteorbrown_big2.png", topleft=(700, 200))]
+# Erzeuge Meteoriten zufällig oben im Spielfeld
+def spawn_meteor(image):
+    meteor = Actor(image)
+    meteor.x = random.randint(50, WIDTH - 50)
+    meteor.y = random.randint(-120, 50)
+    meteor.vy = 0
+    return meteor
+
+meteors = []
+meteor1 = Actor("meteorbrown_big1.png", topleft=(100, 100))
+meteor1.vy = 0
+meteors.append(meteor1)
+
+meteor2 = Actor("meteorbrown_big2.png", topleft=(700, 200))
+meteor2.vy = 0
+meteors.append(meteor2)
 
 # Schüsse
 bullets = []
@@ -49,6 +66,18 @@ def update():
 
     # Schiff bewegen
     ship.x += ship.vx
+    # y Geschwindigkeit meteoriden berechen und anwenden ( Bewegung oben unten)
+    for meteor in meteors:
+        # Gravitation hinzufügen
+        meteor.vy = meteor.vy + GRAVITY
+        # y Bewegung ausführen
+        meteor.y = meteor.y + meteor.vy
+
+        # Meteor oben neu erscheinen lassen, wenn er unten aus dem Bildschirm fällt
+        if meteor.top > HEIGHT:
+            meteor.x = random.randint(50, WIDTH - 50)
+            meteor.y = random.randint(-120, 50)
+            meteor.vy = 0
 
     # Schüsse bewegen
     for bullet in bullets:
